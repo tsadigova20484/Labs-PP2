a55d03864f863cd823bdfa3f0627a5d2795aaf29
tsadigova20484 <tsadigova20484@ada.edu.az>
2026-09-24 15:22:03 +0400

    Update Fact.java
    I added print error if the num is negative

--- Fact.java
+++ Fact.java
@@ -2,11 +2,16 @@
 public class Fact {
     public static void main (String[] args){
         int n =Integer.parseInt(args[0]);
+        if(n<0) {
+            System.out.println("Error");
+        }
+        else[
         int f=1;
         for(int i=1; i<=n; i++) {
             f=f*i;
         }
         System.out.println(f);
+    }
     }
 
 }
