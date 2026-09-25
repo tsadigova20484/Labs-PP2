ca9162d933f45f86fb260a0eeee9db241707d99f
Tutu <tsadigova20484@ada.edu.az>
2026-09-25 11:58:54 +0400

    Add PrintingPatterns

---
+++ PrintingPatterns.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class PrintingPatterns {
+    public static void main (String[] args){
+        Scanner input= new Scanner(System.in);
+        int size = input.nextInt();
+        while(size>0) {
+            if (size % 2 != 0) {
+                System.out.println("* # * # *");
+            }
+            else if (size % 2 == 0) {
+                System.out.println("# * # * #");
+            }
+            size--;
+        }
+
+
+    }
+}
